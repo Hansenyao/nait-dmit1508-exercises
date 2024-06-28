0a93d34df58cb3dd7910d2177ef8cd23cdb4ec02
Youfang Yao <yyfsingler@163.com>
2024-06-27 18:50:43 -0700

    update union exercise sql

--- Ex-IQSchool/Union-Exercise/Union-Exericse-Solution.sql
+++ Ex-IQSchool/Union-Exercise/Union-Exericse-Solution.sql
@@ -1,8 +1,8 @@
-SELECT Staff.StaffID AS 'ID', Staff.FirstName + ' ' + Staff.LastName AS 'Event:Name'
+SELECT Staff.StaffID AS 'ID', CONCAT('Staff Hired:', Staff.FirstName + ' ' + Staff.LastName) AS 'Event:Name'
 FROM Staff
 WHERE DATEPART(mm, Staff.DateHired) = '10'
-UNION
-SELECT Student.StudentID, Student. FirstName + ' ' + Student.LastName
+	UNION
+SELECT Student.StudentID, CONCAT('Student Born:', Student. FirstName + ' ' + Student.LastName) AS 'Event:Name'
 FROM Student
 WHERE DATEPART(mm, Student.Birthdate) = '10'
 ORDER BY [ID] DESC
